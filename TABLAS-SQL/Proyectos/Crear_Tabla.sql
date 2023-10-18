f0228fcc4fd6f1615c010ed0b6b2980526767a7b
maty670 <matiasesposito.10@hotmail.com>
2023-10-18 08:42:24 -0300

    "."

--- TABLAS-SQL/Proyectos/Crear_Tabla.sql
+++ TABLAS-SQL/Proyectos/Crear_Tabla.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS Proyectos (
 	`Modalidad` TEXT CHARACTER SET utf8,
     `Titulo` TEXT CHARACTER SET utf8,
     `Beneficiario` TEXT CHARACTER SET utf8,
-    `Beneficiario_Correo` TEXT CHARACTER SET utf8,
+    `Beneficiario_Correo` VARCHAR(255),
 	`Beneficiario_Departamento` VARCHAR(255),
     `Beneficiario_Localidad` VARCHAR(255) DEFAULT '-',
     `Director` TEXT CHARACTER SET utf8,
     `Director_Correo` TEXT CHARACTER SET utf8,
     `Organizacion_Vinculante` TEXT CHARACTER SET utf8,
-	`Organizacion_Vinculante_Correo` TEXT CHARACTER SET utf8,
+	`Organizacion_Vinculante_Correo` VARCHAR(255),
 	`Palabras_Claves` VARCHAR(255) DEFAULT '-',
     `Monto_ANR` DECIMAL(15,2),
     `Convocatoria` TEXT CHARACTER SET utf8,
@@ -22,7 +22,10 @@ CREATE TABLE IF NOT EXISTS Proyectos (
 -- Definir el valor por defecto
 ALTER TABLE Proyectos
 MODIFY COLUMN Palabras_Claves TEXT,
-MODIFY COLUMN Beneficiario_Localidad TEXT;
+MODIFY COLUMN Beneficiario_Correo TEXT,
+MODIFY COLUMN Beneficiario_Departamento TEXT,
+MODIFY COLUMN Beneficiario_Localidad TEXT,
+MODIFY COLUMN Organizacion_Vinculante_Correo TEXT;
 
 DELIMITER //
 CREATE TRIGGER before_insert_Proyectos
@@ -33,12 +36,20 @@ BEGIN
         SET NEW.Palabras_Claves = '-';
     END IF;
 	
+	IF NEW.Beneficiario_Correo IS NULL THEN
+        SET NEW.Beneficiario_Correo = '-';
+    END IF;
+	
 	IF NEW.Beneficiario_Departamento IS NULL THEN
         SET NEW.Beneficiario_Departamento = '-';
     END IF;
 	
 	IF NEW.Beneficiario_Localidad IS NULL THEN
         SET NEW.Beneficiario_Localidad = '-';
+    END IF;
+	
+	IF NEW.Organizacion_Vinculante_Correo IS NULL THEN
+        SET NEW.Organizacion_Vinculante_Correo = '-';
     END IF;
 	
 END;
